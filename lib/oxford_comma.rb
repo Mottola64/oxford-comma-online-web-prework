5b80f88b02336e9cb11eeb3af626c7b905cad7de
Michael Mottola <mottola64@gmail.com>
2019-02-26 02:00:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-if array.length = 1
-  array.split
-  elsif array.length = 2
-  array.split("and")
+if array.length == 1
+  array.join
+  elsif array.length == 2
+  array.join("and")
 else array.length >= 3
-  array.split(",")
+  array.join(",")
 end
 end
